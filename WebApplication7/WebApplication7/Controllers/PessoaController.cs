30ee0f9d89b2b5a77e7681b74c495f84228ab45f
Bruna Santos <buusantos@hotmail.com>
2023-08-27 21:38:54 -0300

    Comit do projeto
    Comit do projeto completo feito em aula no Visual Studio

---
+++ WebApplication7/WebApplication7/Controllers/PessoaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var pessoa = new Pessoa();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Pessoa pessoa)
+        {
+            //validacao ao chamar a pagina
+           /* if (string.IsNullOrEmpty(pessoa.Nome))
+            {
+                ModelState.AddModelError("Nome", "O campo nome deve ser preenchido");
+            }*/
+
+
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+            return View(pessoa);
+        }
+        public IActionResult Resultado(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+            return View(pessoa);
+        }
+
+
+    }
+}
---
+++ WebApplication7/WebApplication7/Models/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication7.Models
+{
+   
+        public class Pessoa
+        {
+        [Required(ErrorMessage ="Nome deve ser preenchido")]
+            public string Nome { get; set; }
+        [StringLength(50, MinimumLength = 5, ErrorMessage =
+            "Campo deve ter pelo menos 5 caracteres")]
+
+        public string Observacao { get; set; }
+            [Range(18,100, ErrorMessage =
+                "A idade da pessoa deve ter idade entre 18 e 100")]
+
+            public int idade { get; set; }
+
+            public string Email { get; set; }
+
+            public string Login { get; set; }
+
+            public string Senha { get; set; }
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+
+            public string ConfirmarSenha { get; set; }
+
+        
+    }
+}
